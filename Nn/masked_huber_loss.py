0e5254501645de4500f8cb4d494df732c81f9099
doas3140 <dominykas.asauskas@gmail.com>
2018-10-27 14:40:03 +0300

    updated masked huber loss (not usable)

--- Nn/masked_huber_loss.py
+++ Nn/masked_huber_loss.py
@@ -11,15 +11,6 @@ class MaskedHuberLoss():
 		self.criterion = nn.SmoothL1Criterion()
 
 
-	def cuda(self):
-		''' Moves the torch criterion (used for loss and gradient computation)
-			to the GPU.
-		@return the MaskedHuberLoss object that `cuda()` is called on
-		'''
-		self.criterion = self.criterion.cuda()
-		return self
-
-
 	def forward(self, outputs, targets, mask):
 		''' Computes the loss over a batch of neural net outputs and targets.
 		@param: outputs an (N,M) tensor containing N vectors of values over buckets,
@@ -31,15 +22,29 @@ class MaskedHuberLoss():
 		@return the sum of Huber loss applied elementwise on `outputs` and `targets`,
 				masked so that only valid buckets are included
 		'''
-		pass
+		batch_size = outputs.shape[0]
+		feature_size = outputs.shape[1]
 		# 1.0 zero out the outputs/target so that the error does not depend on these
-		# 2.0 if the batch size has changed, create new storage for the sum,
-		# otherwise reuse
+		outputs *= mask
+  		targets *= mask
+		loss = self.criterion.forward(outputs, targets)
+		# 2.0 if the batch size has changed, create new storage for the sum, otherwise reuse
+		if self.mask_sum is None or (self.mask_sum.shape[0] != batch_size):
+			self.mask_placeholder = np.zeros(mask.shape, dtype=arguments.dtype)
+			self.mask_sum = np.zeros([batch_size], dtype=arguments.dtype)
+			self.mask_multiplier = self.mask_sum.copy().reshape([-1,1])
 		# 3.0 compute mask sum for each batch
+		self.mask_placeholder = mask.copy()
+		self.mask_sum = np.sum(self.mask_placeholder, axis=1)
 		# 3.1 mask multiplier - note that mask is 1 for impossible features
-		# 4.0 multiply to get a new losss
+		self.mask_multiplier.fill(feature_size)
+		self.mask_multiplier -= self.mask_sum
+		self.mask_multiplier /= feature_size
+		# 4.0 multiply to get a new loss
 		# loss is not really computed batch-wise correctly,
     	# but that does not really matter now since gradients are correct
+		loss_multiplier = (batch_size * feature_size) / (batch_size * feature_size - self.mask_sum.sum() )
+		new_loss = loss_multiplier * loss
 
 
 
@@ -55,8 +60,10 @@ class MaskedHuberLoss():
 				@{bucket_conversion.get_possible_bucket_mask}
 		@return the gradient of @{forward} applied to the arguments
 		'''
-		pass
+		dloss_doutput = self.criterion.backward(outputs, targets)
 		# we use the multiplier computed with the mask during forward call
+		dloss_doutput /= self.mask_multiplier:expandAs(dloss_doutput)
+		return dloss_doutput
 
 
 
