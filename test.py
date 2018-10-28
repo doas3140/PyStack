8fef1bee14026e1f678c8814a138b7b0b0a2c544
doas3140 <dominykas.asauskas@gmail.com>
2018-10-28 12:43:19 +0200

    updated tests

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ from Tree.tree_values import TreeValues
 from Tree.tree_cfr import TreeCFR
 from Settings.constants import constants
 from Settings.game_settings import game_settings
+from Settings.arguments import arguments
 from Game.card_tools import card_tools
 from Game.card_to_string_conversion import card_to_string
 from helper_classes import TreeParams, Node
 import numpy as np
+from time import time
 
+PC, CC = constants.players_count, game_settings.card_count
 
 params = TreeParams()
 
@@ -20,18 +23,20 @@ params.root_node.bets = np.array([100, 100])
 
 tree = tree_builder.build_tree(params)
 
-PC, CC = constants.players_count, game_settings.card_count
-starting_ranges = np.zeros([PC,CC],dtype=float)
-
+starting_ranges = np.zeros([PC,CC], dtype=arguments.dtype)
 starting_ranges[0] = card_tools.get_uniform_range(params.root_node.board)
 starting_ranges[1] = card_tools.get_uniform_range(params.root_node.board)
 
+t0 = time()
 tree_cfr = TreeCFR()
 tree_cfr.run_cfr(tree, starting_ranges)
+print(time()-t0)
 
 tree_values = TreeValues()
 tree_values.compute_values(tree, starting_ranges)
 
 print('Exploitability: ' + str(tree.exploitability) + ' [chips]' )
 
-tree_visualizer.draw_tree(tree)
+print(np.array2string(tree.strategy, suppress_small=True, precision=2))
+
+# tree_visualizer.draw_tree(tree)
