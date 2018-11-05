bb9d5df1c47d8ebb8489a88ff49a8b3f8b7900ce
doas3140 <dominykas.asauskas@gmail.com>
2018-11-05 21:03:45 +0200

    init mock_resolving

--- Lookahead/mock_resolving.py
+++ Lookahead/mock_resolving.py
@@ -4,15 +4,15 @@
 	Used for debugging.
 '''
 
-from ..Lookahead.lookahead import Lookahead
-from ..Lookahead.cfrd_gadget import CFRDGadget
-# from ..Tree.tree_builder import PokerTreeBuilder # dont need here?
-# from ..Tree.tree_visualiser import TreeVisualiser # dont need here?
-from ..Settings.arguments import arguments
-from ..Settings.constants import constants
-from ..tools import tools
-from ..Game.card_tools import card_tools
-from ..Settings.game_settings import game_settings
+from Lookahead.lookahead import Lookahead
+from Lookahead.cfrd_gadget import CFRDGadget
+# from Tree.tree_builder import PokerTreeBuilder # dont need here?
+# from Tree.tree_visualiser import TreeVisualiser # dont need here?
+from Settings.arguments import arguments
+from Settings.constants import constants
+from tools import tools
+from Game.card_tools import card_tools
+from Settings.game_settings import game_settings
 
 class MockResolving():
 	def __init__(self):
@@ -25,7 +25,8 @@ class MockResolving():
 		@param: [opt] opponent_range not used
 		@see resolving.resolve_first_node
 		'''
-		pass
+		self.node = node
+		self.action_count = self.node.actions.shape[0]
 
 
 	def resolve(self, node, player_range, opponent_cfvs):
@@ -35,7 +36,8 @@ class MockResolving():
 		@param: [opt] opponent_cfvs not used
 		@see resolving.resolve
 		'''
-		pass
+		self.node = node
+		self.action_count = self.node.actions.shape[0]
 
 
 	def get_possible_actions(self):
@@ -43,7 +45,7 @@ class MockResolving():
 		@return the actions that can be taken at the re-solve node
 		@see resolving.get_possible_actions
 		'''
-		pass
+		return self.node.actions
 
 
 	def get_root_cfv(self):
@@ -51,7 +53,8 @@ class MockResolving():
 		@return a vector of 1s
 		@see resolving.get_root_cfv
 		'''
-		pass
+		CC = game_settings.card_count
+		return np.ones([CC], dtype=arguments.dtype)
 
 
 	def get_action_cfv(self, action):
@@ -60,7 +63,8 @@ class MockResolving():
 		@return a vector of 1s
 		@see resolving.get_action_cfv
 		'''
-		pass
+		CC = game_settings.card_count
+		return np.ones([CC], dtype=arguments.dtype)
 
 
 	def get_chance_action_cfv(self, player_action, board):
@@ -70,7 +74,8 @@ class MockResolving():
 		@return a vector of 1s
 		@see resolving.get_chance_action_cfv
 		'''
-		pass
+		CC = game_settings.card_count
+		return np.ones([CC], dtype=arguments.dtype)
 
 
 	def get_action_strategy(self, action):
@@ -79,7 +84,8 @@ class MockResolving():
 		@return a vector of 1s
 		@see resolving.get_action_strategy
 		'''
-		pass
+		CC = game_settings.card_count
+		return np.ones([CC], dtype=arguments.dtype)
 
 
 
