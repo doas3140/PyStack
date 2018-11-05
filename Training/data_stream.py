b29f203e63f93125ee94e42aaf984ba478d5c5dd
doas3140 <dominykas.asauskas@gmail.com>
2018-11-05 22:28:24 +0200

    init Training

--- Training/data_stream.py
+++ Training/data_stream.py
@@ -2,17 +2,33 @@
 	Handles the data used for neural net training and validation.
 '''
 
-from ..Settings.arguments import arguments
+from Settings.arguments import arguments
+from helper_classes import Data
 
 class DataStream():
 	def __init__(self):
 		''' Reads the data from training and validation files generated with
 			@{data_generation_call.generate_data}.
 		'''
-		pass
 		# loadind valid data
+		self.data = Data()
+		valid_prefix = arguments.data_path + 'valid'
+		self.data.valid_mask= np.load(valid_prefix + '.mask')
+		self.data.valid_mask= self.data.valid_mask:repeatTensor(1,2)
+		self.data.valid_targets = np.load(valid_prefix + '.targets')
+		self.data.valid_inputs = np.load(valid_prefix + '.inputs')
+		self.valid_data_count = self.data.valid_inputs.shape[0]
+		assert(self.valid_data_count >= arguments.train_batch_size, 'Validation data count has to be greater than a train batch size!')
+		self.valid_batch_count = self.valid_data_count / arguments.train_batch_size
 		# loading train data
-		# transfering data to gpu if needed
+		train_prefix = arguments.data_path + 'train'
+		self.data.train_mask = np.load(train_prefix + '.mask')
+		self.data.train_mask = self.data.train_mask:repeatTensor(1,2)
+		self.data.train_inputs = np.load(train_prefix + '.inputs')
+		self.data.train_targets = np.load(train_prefix + '.targets')
+		self.train_data_count = self.data.train_inputs.shape[0]
+		assert(self.train_data_count >= arguments.train_batch_size, 'Training data count has to be greater than a train batch size!')
+		self.train_batch_count = self.train_data_count / arguments.train_batch_size
 
 
 	def get_valid_batch_count(self):
@@ -20,7 +36,7 @@ class DataStream():
 			Batch size is defined by @{arguments.train_batch_size}.
 		@return the number of batches
 		'''
-		pass
+		return self.valid_batch_count
 
 
 	def get_train_batch_count(self):
@@ -28,7 +44,7 @@ class DataStream():
 			Batch size is defined by @{arguments.train_batch_size}
 		@return the number of batches
 		'''
-		pass
+		return self.train_batch_count
 
 
 	def start_epoch(self):
@@ -36,8 +52,16 @@ class DataStream():
 			Done so that the data is encountered in a different order
 			for each epoch.
 		'''
-		pass
 		# data are shuffled each epoch
+		shuffled_idx = np.arange(len(self.train_data_count))
+		np.random.shuffle(shuffled_idx)
+		self.data.train_inputs = self.data.train_inputs[shuffled_idx]
+		self.data.train_targets = self.data.train_targets[shuffled_idx]
+		self.data.train_mask = self.data.train_mask[shuffled_idx]
+		# shuffle = torch.randperm(self.train_data_count).long()
+		# self.data.train_inputs = self.data.train_inputs.index(1, shuffle)
+		# self.data.train_targets = self.data.train_targets.index(1, shuffle)
+		# self.data.train_mask = self.data.train_mask.index(1, shuffle)
 
 
 	def get_batch(self, inputs, targets, mask, batch_index):
@@ -50,7 +74,13 @@ class DataStream():
 		@return the targets set for the batch
 		@return the masks set for the batch
 		'''
-		pass
+		assert(inputs:size(1) == targets:size(1) and inputs:size(1) == mask:size(1))
+		batch_boundaries = ( (batch_index-1)*arguments.train_batch_size+1, batch_index*arguments.train_batch_size )
+		b_start, b_end = batch_boundaries
+		batch_inputs = inputs[ b_start:b_end, : ]
+		batch_targets = targets[ b_start:b_end, : ]
+		batch_mask = mask[ b_start:b_end, : ]
+		return batch_inputs, batch_targets, batch_mask
 
 
 	def get_train_batch(self, batch_index):
@@ -60,7 +90,7 @@ class DataStream():
 		@return the targets set for the batch
 		@return the masks set for the batch
 		'''
-		pass
+		return self.get_batch(self.data.train_inputs, self.data.train_targets, self.data.train_mask, batch_index)
 
 
 	def get_valid_batch(self, batch_index):
@@ -70,7 +100,7 @@ class DataStream():
 		@return the targets set for the batch
 		@return the masks set for the batch
 		'''
-		pass
+		return self.get_batch(self.data.valid_inputs, self.data.valid_targets, self.data.valid_mask, batch_index)
 
 
 
--- Training/main_train.py
+++ Training/main_train.py
@@ -3,14 +3,13 @@
 	Uses data previously generated with @{data_generation_call}.
 '''
 
-from ..Nn.net_builder import nnBuilder
-from ..Training.data_stream import DataStream
-from ..Training.train import train
-from ..Settings.arguments import arguments
+from Nn.net_builder import nnBuilder
+from Training.data_stream import DataStream
+from Training.train import train
+from Settings.arguments import arguments
 
 def main():
 	pass
-	# build network
 
 
 
