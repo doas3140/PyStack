6bce5bfbd58532a9ae78e19a1ee93b04c933d10e
doas3140 <dominykas.asauskas@gmail.com>
2018-11-05 21:00:30 +0200

    init resolving

--- Lookahead/resolving.py
+++ Lookahead/resolving.py
@@ -3,14 +3,12 @@
 	Internally uses @{cfrd_gadget|CFRDGadget} TODO SOLVER
 '''
 
-from ..Lookahead.lookahead import Lookahead
-from ..Lookahead.cfrd_gadget import CFRDGadget
-from ..Tree.tree_builder import PokerTreeBuilder
-# from ..Tree.tree_visualiser import TreeVisualiser # dont need here?
-from ..Settings.arguments import arguments
-from ..Settings.constants import constants
-from ..tools import tools
-from ..Game.card_tools import card_tools
+from Lookahead.lookahead import Lookahead
+from Lookahead.cfrd_gadget import CFRDGadget
+from Tree.tree_builder import PokerTreeBuilder
+from Settings.arguments import arguments
+from Settings.constants import constants
+from Game.card_tools import card_tools
 
 class Resolving():
 	def __init__(self):
@@ -21,7 +19,10 @@ class Resolving():
 		''' Builds a depth-limited public tree rooted at a given game node.
 		@param: node the root of the tree
 		'''
-		pass
+		build_tree_params = {}
+		build_tree_params.root_node = node
+		build_tree_params.limit_to_street = true
+		self.lookahead_tree = self.tree_builder.build_tree(build_tree_params)
 
 
 	def resolve_first_node(self, node, player_range, opponent_range):
@@ -33,7 +34,12 @@ class Resolving():
 		@param: player_range a range vector for the re-solving player
 		@param: opponent_range a range vector for the opponent
 		'''
-		pass
+		self._create_lookahead_tree(node)
+		self.lookahead = Lookahead()
+		self.lookahead.build_lookahead(self.lookahead_tree)
+		self.lookahead.resolve_first_node(player_range, opponent_range)
+		self.resolve_results = self.lookahead.get_results()
+		return self.resolve_results
 
 
 	def resolve(self, node, player_range, opponent_cfvs):
@@ -44,7 +50,13 @@ class Resolving():
 			@param: opponent_cfvs a vector of cfvs achieved by the opponent
 					before re-solving
 		'''
-		pass
+		assert(card_tools.is_valid_range(player_range, node.board))
+		self._create_lookahead_tree(node)
+		self.lookahead = Lookahead()
+		self.lookahead.build_lookahead(self.lookahead_tree)
+		self.lookahead.resolve(player_range, opponent_cfvs)
+		self.resolve_results = self.lookahead:get_results()
+		return self.resolve_results
 
 
 	def _action_to_action_id(self, action):
@@ -53,7 +65,13 @@ class Resolving():
 		@param: action a legal action at the node
 		@return the index of the action
 		'''
-		pass
+		actions = self:get_possible_actions(action)
+		action_id = -1
+		for i in range(1, actions.shape[0]+1):
+			if action == actions[i]:
+				action_id = i
+		assert(action_id != -1)
+		return action_id
 
 
 	def get_possible_actions(self):
@@ -61,7 +79,7 @@ class Resolving():
  			The node must first be re-solved with @{resolve} or @{resolve_first_node}.
 		@return a list of legal actions
 		'''
-		pass
+		return self.lookahead_tree.actions
 
 
 	def get_root_cfv(self):
@@ -70,7 +88,7 @@ class Resolving():
 			The node must first be re-solved with @{resolve_first_node}.
 		@return a vector of cfvs
 		'''
-		pass
+		return self.resolve_results.root_cfvs
 
 
 	def get_root_cfv_both_players(self):
@@ -80,7 +98,7 @@ class Resolving():
 			The node must first be re-solved with @{resolve_first_node}.
 		@return a (2,K) tensor of cfvs, where K is the range size
 		'''
-		pass
+		return self.resolve_results.root_cfvs_both_players
 
 
 	def get_action_cfv(self, action):
@@ -92,7 +110,8 @@ class Resolving():
 				at the node being re-solved
 		@return a vector of cfvs
 		'''
-		pass
+		action_id = self._action_to_action_id(action)
+		return self.resolve_results.children_cfvs[action_id]
 
 
 	def get_chance_action_cfv(self, action, board):
@@ -107,7 +126,8 @@ class Resolving():
 				which were updated by the chance event
 		@return a vector of cfvs
 		'''
-		pass
+		action_id = self._action_to_action_id(action)
+		return self.lookahead:get_chance_action_cfv(action_id, board)
 
 
 	def get_action_strategy(self, action):
@@ -117,7 +137,8 @@ class Resolving():
 		@return a vector giving the probability of taking the action
 				with each private hand
 		'''
-		pass
+		action_id = self._action_to_action_id(action)
+		return self.resolve_results.strategy[action_id]
 
 
 
