c2d2f56a19789a304aa458921eba42bf7bb6cc61
doas3140 <dominykas.asauskas@gmail.com>
2018-11-27 17:03:09 +0200

    init data generation

--- DataGeneration/data_generation.py
+++ DataGeneration/data_generation.py
@@ -8,8 +8,8 @@ from tqdm import tqdm
 
 from Settings.arguments import arguments
 from Settings.game_settings import game_settings
-from DataGeneration.random_card_generator import card_generator
 from Settings.constants import constants
+from Game.card_to_string_conversion import card_to_string
 from DataGeneration.range_generator import RangeGenerator
 from Nn.bucketer import Bucketer
 from Nn.bucket_conversion import BucketConversion
@@ -22,99 +22,93 @@ class DataGeneration():
 	def __init__(self):
 		self.dirpath = os.path.join(arguments.data_path, 'npy')
 		self.counter = 0
+		# init range generator and term eq
+		self.range_generator = RangeGenerator()
+		self.term_eq = TerminalEquity()
+		# main vars
+		HC, PC = game_settings.hand_count, constants.players_count
+		self.target_size = HC * PC
+		self.input_size = HC * PC + 1
 
 
-	def generate_data(self, data_count, num_files=1):
-		''' Generates training and validation files by sampling random poker
-			situations and solving them.
-			Makes two calls to @{generate_data_file}. The files are saved to
-			@{arguments.data_path}, respectively appended with `valid` and `train`.
-		@param: the number of training examples to generate per file
-		@param: number files to create (total examples: num_files x data_count)
-		'''
-		for self.counter in range(num_files):
-			t0 = time.time()
-			print('Generating data... (iteration:{})'.format(self.counter))
-			self.generate_data_file(data_count)
-			print('Gen time:', time.time() - t0)
+	def solve_board(self, board, batch_size):
+		HC, PC = game_settings.hand_count, constants.players_count
+		# set board in terminal equity and range generator
+		t0 = time.time()
+		self.term_eq.set_board(board)
+		self.range_generator.set_board(self.term_eq, board)
+		# init inputs and outputs
+		targets = np.zeros([batch_size, self.target_size], dtype=arguments.dtype)
+		inputs = np.zeros([batch_size, self.input_size], dtype=arguments.dtype)
+		# generating ranges
+		ranges = np.zeros([PC, batch_size, HC], dtype=arguments.dtype)
+		for player in range(PC):
+			self.range_generator.generate_range(ranges[player])
+		# put generated ranges into inputs
+		for p in range(PC):
+			inputs[ : , p*HC:(p+1)*HC ] = ranges[p]
+		# generating pot sizes between ante and stack - 0.1
+		pot_intervals = [(100,100), (200,400), (400,2000), (2000,6000), (6000,18000)]
+		# take random pot size
+		random_interval = pot_intervals[ np.random.randint(len(pot_intervals)) ]
+		random_pot_size = int( np.random.uniform(low=random_interval[0], high=random_interval[1]) )
+		# pot features are pot sizes normalized between [ante/stack; 1]
+		normalized_pot_size = random_pot_size / arguments.stack
+		# put normalized pot size into inputs
+		inputs[ : , -1 ].fill(normalized_pot_size)
+		# set up solver
+		resolving = Resolving(self.term_eq, verbose=0)
+		# setting up first node
+		current_node = Node()
+		current_node.board = board
+		current_node.street = self.street
+		current_node.num_bets = 0
+		current_node.current_player = constants.players.P1 if self.street == 1 else constants.players.P2
+		# TODO support preflop bets
+		current_node.bets = np.array([random_pot_size, random_pot_size], dtype=arguments.dtype)
+		# solve this node and return cfvs of root node
+		resolving.resolve(current_node, player_range=ranges[0], opponent_range=ranges[1])
+		root_values = resolving.get_root_cfv_both_players() # [b, P, I]
+		# normalize cfvs dividing by pot size
+		root_values /= random_pot_size
+		# put calculated cfvs into targets
+		for p in range(PC):
+			targets[ : , p*HC:(p+1)*HC ] = root_values[ : , p , : ]
+		# return inputs [b, I x P + 1] and targets [b, I x P]
+		return inputs, targets
 
 
-	def generate_data_file(self, data_count):
-		''' Generates data files containing examples of random poker situations with
-			counterfactual values from an associated solution.
-			Each poker situation is randomly generated using @{range_generator} and
-			@{random_card_generator}. For description of neural net input and target
-			type, see @{net_builder}.
-		@param: data_count the number of examples to generate
-		'''
-		BS, PC = arguments.gen_batch_size, constants.players_count
-		BCC, CC = game_settings.board_card_count, game_settings.card_count
-		range_generator = RangeGenerator()
-		assert(data_count % BS == 0, 'data count has to be divisible by the batch size')
-		batch_count = int(data_count / BS)
-		bucketer = Bucketer()
-		bucket_count = bucketer.get_bucket_count()
-		bC = bucket_count
-		target_size = bC * PC
-		targets = np.zeros([data_count, target_size], dtype=arguments.dtype)
-		input_size = bC * PC + 1
-		inputs = np.zeros([data_count, input_size], dtype=arguments.dtype)
-		masks = np.zeros([data_count, bC], dtype=np.uint8)
-		bucket_conversion = BucketConversion()
-		for b in tqdm(range(batch_count)):
-			board = card_generator.generate_cards(BCC)
-			range_generator.set_board(board)
-			bucket_conversion.set_board(board)
-			# generating ranges
-			ranges = np.zeros([PC, BS, CC], dtype=arguments.dtype)
-			for player in range(PC):
-				range_generator.generate_range(ranges[player])
-				# just simple random card generator below
-				# prob = np.random.rand(BS,CC)
-				# ranges[player] = prob / np.sum(prob, axis=1).reshape([BS,1])
-			# generating pot sizes between ante and stack - 0.1
-			min_pot = arguments.ante
-			max_pot = arguments.stack - 0.1
-			pot_range = max_pot - min_pot
-			random_pot_sizes = np.random.rand(BS,1) * pot_range + min_pot # (BS,1)
-			# pot features are pot sizes normalized between (ante/stack,1)
-			pot_size_features = random_pot_sizes.copy() * (1/arguments.stack)
-			# translating ranges to features
-			pot_feature_index = -1
-			inputs[ b*BS:(b+1)*BS, pot_feature_index ] = pot_size_features.reshape(inputs[ b*BS:(b+1)*BS, pot_feature_index ].shape)
-			player_indexes = [ (0,bC), (bC,bC*2) ]
-			for player in range(PC):
-				start_idx, end_idx = player_indexes[player]
-				bucket_conversion.card_range_to_bucket_range(ranges[player], inputs[ b*BS:(b+1)*BS , start_idx:end_idx ])
-			# computaton of values using re-solving
-			values = np.zeros([PC, BS, CC], dtype=arguments.dtype)
-			for i in range(BS):
-				resolving = Resolving()
-				current_node = Node()
-				current_node.board = board
-				current_node.street = 2
-				current_node.current_player = constants.players.P1
-				pot_size = pot_size_features[i][0] * arguments.stack
-				current_node.bets = np.array([pot_size, pot_size])
-				p1_range = ranges[0][i]
-				p2_range = ranges[1][i]
-				resolving.resolve_first_node(current_node, p1_range, p2_range)
-				root_values = resolving.get_root_cfv_both_players()
-				root_values *= 1 / pot_size
-				values[ : , i , : ] = root_values
-			# translating values to nn targets
-			for player in range(PC):
-				start_idx, end_idx = player_indexes[player]
-				bucket_conversion.card_range_to_bucket_range(values[player], targets[ b*BS:(b+1)*BS , start_idx:end_idx ])
-			# computing a mask of possible buckets
-			bucket_mask = bucket_conversion.get_possible_bucket_mask()
-			masks[ b*BS:(b+1)*BS , : ] = bucket_mask * np.ones([BS,bC], dtype=np.uint8)
-		fpath = os.path.join(self.dirpath, '{}.{}')
-		np.save(fpath.format('inputs', self.counter), inputs.astype(np.float32))
-		np.save(fpath.format('targets', self.counter), targets.astype(np.float32))
-		np.save(fpath.format('masks', self.counter), masks.astype(np.uint8))
 
-
+	def generate_data(self, street, starting_idx=0):
+		card_count = game_settings.card_count
+		# set up scalar variables
+		self.street = street
+		num_board_cards = game_settings.board_card_count[self.street-1]
+		batch_size = arguments.gen_batch_size
+		num_different_boards = arguments.gen_different_boards
+		total_situations = batch_size * num_different_boards
+		num_files = arguments.gen_num_files
+		num_batches_in_file = total_situations // num_files
+		for self.counter in range(starting_idx,  starting_idx + num_files):
+			TARGETS = np.zeros([num_batches_in_file, self.target_size], dtype=arguments.dtype)
+			INPUTS =  np.zeros([num_batches_in_file, self.input_size],  dtype=arguments.dtype)
+			BOARDS = np.zeros([num_different_boards, num_board_cards], dtype=arguments.dtype)
+			for b in range(num_different_boards):
+				# t0 = time.time()
+				# init targets, inputs and create random board and solve it
+				board = np.random.choice(card_count, size=num_board_cards, replace=False)
+				inputs, targets = self.solve_board(board, batch_size)
+				# save to placeholders for later
+				TARGETS[ b*batch_size:(b+1)*batch_size , : ] = targets
+				INPUTS[ b*batch_size:(b+1)*batch_size , : ] = inputs
+				BOARDS[ b , : ] = board
+				# print('{}, {}) took {} seconds'.format(self.counter, b, time.time()-t0))
+			# save
+			fpath = os.path.join(self.dirpath, '{}.{}')
+			np.save(fpath.format('inputs', self.counter), INPUTS.astype(np.float32))
+			np.save(fpath.format('targets', self.counter), TARGETS.astype(np.float32))
+			np.save(fpath.format('boards', self.counter), BOARDS.astype(np.uint8))
+			self.counter += 1
 
 
 data_generation = DataGeneration()
--- DataGeneration/main_data_generation.py
+++ DataGeneration/main_data_generation.py
@@ -1,5 +1,5 @@
 '''
-	Script that generates training and validation files.
+	Script that generates ranges and cfvs.
 '''
 import sys
 import os
@@ -8,4 +8,46 @@ sys.path.append(os.getcwd())
 from Settings.arguments import arguments
 from DataGeneration.data_generation import data_generation
 
-data_generation.generate_data(arguments.gen_data_count, arguments.gen_num_files)
+AVAILABLE_STREETS = [1,4]
+
+error = Exception(''' Please specify the street.
+
+	examples:
+	python -m DataGeneration/main_data_generation.py --street 4
+	python -m DataGeneration/main_data_generation.py --street=4
+
+	available streets:
+	1: preflop
+	4: river
+
+	setting starting idx of filenames:
+	python -m DataGeneration/main_data_generation.py --street 4 --start-idx 1
+	''')
+
+def search_argument(name, args):
+	for i, arg in enumerate(args):
+		if name in arg:
+			if '=' in arg:
+				possible_result = arg.split('=')[-1]
+			else:
+				possible_result = args[i+1]
+			try:
+				return int(possible_result)
+			except:
+				raise(error)
+	return None
+
+def parse_arguments(args):
+	street = search_argument('--street', args)
+	idx = search_argument('--start-idx', args)
+	if street is None or street not in AVAILABLE_STREETS:
+		raise(error)
+	if idx is None:
+		idx = 0
+	return street, idx
+
+
+args = sys.argv[1:]
+street, starting_idx = parse_arguments(args)
+
+data_generation.generate_data(street, starting_idx)
