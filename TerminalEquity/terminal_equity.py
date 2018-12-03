a381aa14a4522024762b0fac412f285d537b468f
doas3140 <dominykas.asauskas@gmail.com>
2018-12-03 21:15:04 +0200

    fixed terminal equity

--- TerminalEquity/terminal_equity.py
+++ TerminalEquity/terminal_equity.py
@@ -8,18 +8,17 @@ from Settings.game_settings import game_settings
 from Settings.arguments import arguments
 from Settings.constants import constants
 from Game.card_tools import card_tools
+from tools import tools
 
 class TerminalEquity():
 	def __init__(self):
 		self._block_matrix = np.load('TerminalEquity/block_matrix.npy')
 		self._pf_equity = np.load('TerminalEquity/pf_equity.npy')
-		self.equity_matrix = None # (CC,CC), can be named as call matrix
-		self.fold_matrix = None # (CC,CC)
-		self.matrix_mem = None #
-		self.batch_size = 10
+		self.equity_matrix = None # [I,I] can be named as call matrix
+		self.fold_matrix = None # [I,I]
 
 
-	def get_last_round_call_matrix(self, board_cards, call_matrix):
+	def set_last_round_call_matrix(self, call_matrix, board_cards):
 		''' Constructs the matrix that turns player ranges into showdown equity.
 			Gives the matrix `A` such that for player ranges `x` and `y`, `x'Ay`
 			is the equity for the first player when no player folds.
@@ -31,46 +30,47 @@ class TerminalEquity():
 			assert(board_cards.shape[0] == 1 or board_cards.shape[0] == 2 or board_cards.shape[0] == 5) # Only Leduc, extended Leduc, and Texas Holdem are supported
 		strength = evaluator.batch_eval_fast(board_cards)
 		# handling hand stregths (winning probs)
-		strength_view_1 = strength.reshape([HC,1]) * np.ones_like(call_matrix) # ? galima broadcastint
-		strength_view_2 = strength.reshape([1,HC]) * np.ones_like(call_matrix)
+		strength_view_1 = strength.reshape([HC,1]) # * np.ones_like(call_matrix)
+		strength_view_2 = strength.reshape([1,HC]) # * np.ones_like(call_matrix)
 
-		call_matrix[:,:] = (strength_view_1 > strength_view_2).astype(int)
+		call_matrix[:,:]  = (strength_view_1 > strength_view_2).astype(int)
 		call_matrix[:,:] -= (strength_view_1 < strength_view_2).astype(int)
-		self._handle_blocking_cards(call_matrix, board_cards)
 
 
-	def get_inner_call_matrix(self, board_cards, call_matrix): # TODO: can be problems
+	def set_inner_call_matrix(self, call_matrix, next_round_boards, street):
 		''' Constructs the matrix that turns player ranges into showdown equity.
 			Gives the matrix `A` such that for player ranges `x` and `y`, `x'Ay`
 			is the equity for the first player when no player folds.
-		@param board_cards a non-empty vector of board cards
+		@param next_round_boards [b,5] a non-empty vector of board cards
 		@param call_matrix a tensor where the computed matrix is stored
 		'''
-		HC, num_boards = game_settings.hand_count, board_cards.shape[0]
-		if board_cards.ndim != 0:
-			assert(board_cards.shape[0] == 1 or board_cards.shape[0] == 2 or board_cards.shape[0] == 5) # Only Leduc, extended Leduc, and Texas Holdem are supported
-		strength = evaluator.batch_eval_fast(board_cards)
+		HC, num_boards = game_settings.hand_count, next_round_boards.shape[0]
+		BCC, CC = game_settings.board_card_count, game_settings.card_count
+		if next_round_boards.ndim != 0:
+			assert(next_round_boards.shape[1] == 0 or next_round_boards.shape[1] == 2 or next_round_boards.shape[1] == 5) # Only Leduc, extended Leduc, and Texas Holdem are supported
+		strength = evaluator.batch_eval_fast(next_round_boards) # [b,I]
 		# handling hand stregths (winning probs)
-		strength_view_1 = strength.reshape([num_boards,HC,1]) * np.ones([num_boards, HC, HC], dtype=strength.dtype) # ? galima broadcastint
+		strength_view_1 = strength.reshape([num_boards,HC,1]) * np.ones([num_boards, HC, HC], dtype=strength.dtype)
 		strength_view_2 = strength.reshape([num_boards,1,HC]) * np.ones_like(strength_view_1)
-		possible_mask = (strength < 0).astype(call_matrix.dtype)
-		for i in range(0, num_boards+1, self.batch_size): # ? - kodel +1
-			i1, i2 = i, i + self.batch_size
-			bs = self.batch_size
-			if i + self.batch_size > num_boards:
-				i1, i2 = i, num_boards
-				bs = num_boards - i
-			self.matrix_mem[  :bs ] = (strength_view_1[ i1:i2 ] > strength_view_2[ i1:i2 ]).copy()
-			self.matrix_mem[  :bs ] *= possible_mask[ i1:i2 ].reshape([bs,1,HC]) * np.ones([bs,HC,HC], dtype=possible_mask.dtype)
-			self.matrix_mem[  :bs ] *= possible_mask[ i1:i2 ].reshape([bs,HC,1]) * np.ones([bs,HC,HC], dtype=possible_mask.dtype)
-			call_matrix[:,:] += np.sum(self.matrix_mem[  :bs ], axis=0) # ? - keepdims
+		possible_mask = (strength < 0).astype(int)
 
-			self.matrix_mem[  :bs ] = (strength_view_1[ i1:i2 ] < strength_view_2[ i1:i2 ]).copy()
-			self.matrix_mem[  :bs ] *= possible_mask[ i1:i2 ].reshape([bs,1,HC]) * np.ones([bs,HC,HC], dtype=possible_mask.dtype)
-			self.matrix_mem[  :bs ] *= possible_mask[ i1:i2 ].reshape([bs,HC,1]) * np.ones([bs,HC,HC], dtype=possible_mask.dtype)
-			call_matrix[:,:] -= np.sum(self.matrix_mem[  :bs ], axis=0) # ? - keepdims
+		matrix_mem = (strength_view_1 > strength_view_2).astype(int)
+		matrix_mem *= possible_mask.reshape([num_boards,1,HC]) # * np.ones([num_boards,HC,HC], dtype=possible_mask.dtype)
+		matrix_mem *= possible_mask.reshape([num_boards,HC,1]) # * np.ones([num_boards,HC,HC], dtype=possible_mask.dtype)
+		call_matrix[:,:] = np.sum(matrix_mem, axis=0)
 
-		self._handle_blocking_cards(call_matrix, board_cards)
+		matrix_mem = (strength_view_1 < strength_view_2).astype(int)
+		matrix_mem *= possible_mask.reshape([num_boards,1,HC]) # * np.ones([num_boards,HC,HC], dtype=possible_mask.dtype)
+		matrix_mem *= possible_mask.reshape([num_boards,HC,1]) # * np.ones([num_boards,HC,HC], dtype=possible_mask.dtype)
+		call_matrix[:,:] = call_matrix - np.sum(matrix_mem, axis=0)
+		# normalize sum
+		num_cards_on_board = BCC[street-1]
+		num_cards_in_hand = game_settings.hand_card_count
+		num_players = constants.players_count
+		cards_to_come = BCC[-1] - num_cards_on_board
+		cards_left = CC - (num_cards_in_hand * num_players + num_cards_on_board)
+		num_possible_hands = tools.choose(cards_left, cards_to_come)
+		call_matrix[:,:] = call_matrix / num_possible_hands
 
 
 
@@ -80,62 +80,11 @@ class TerminalEquity():
 		@param: equity_matrix the matrix to modify
 		@param: board a possibly empty vector of board cards
 		'''
-		HC, HCC, CC = game_settings.hand_count, game_settings.hand_card_count, game_settings.card_count
-		possible_hand_indexes = card_tools.get_possible_hand_indexes(board) # (CC,) bool type
+		HC, CC = game_settings.hand_count, game_settings.card_count
+		possible_hand_indexes = card_tools.get_possible_hand_indexes(board)
 		equity_matrix[:,:] *= possible_hand_indexes.reshape([1,HC])
-		equity_matrix[:,:] *= possible_hand_indexes.reshape([HC,1]) # np.dot can be faster
-		if HCC == 2:
-			equity_matrix[:,:] *= self._block_matrix
-		elif HCC == 1:
-			for i in range(CC):
-				equity_matrix[i,i] = 0
-
-
-	def _set_fold_matrix(self, board):
-		''' Sets the evaluator's fold matrix, which gives the equity for terminal
-			nodes where one player has folded.
-			Creates the matrix `B` such that for player ranges `x` and `y`,
-			`x'By` is the equity for the player who doesn't fold
-		@param: board a possibly empty vector of board cards
-		'''
-		HC = game_settings.hand_count
-		self.fold_matrix = np.ones([HC,HC], dtype=arguments.dtype)
-		# setting cards that block each other to zero
-		self._handle_blocking_cards(self.fold_matrix, board)
-
-
-	def _set_call_matrix(self, board):
-		''' Sets the evaluator's call matrix, which gives the equity for terminal
-			nodes where no player has folded.
-			For nodes in the last betting round, creates the matrix `A` such that
-			for player ranges `x` and `y`, `x'Ay` is the equity for the first
-			player when no player folds. For nodes in the first betting round,
-			gives the weighted average of all such possible matrices.
-		@param: board a possibly empty vector of board cards
-		'''
-		HC, HCC = game_settings.hand_count, game_settings.hand_card_count
-		BCC, SC = game_settings.board_card_count, constants.streets_count
-		CC = game_settings.card_count
-		street = card_tools.board_to_street(board)
-		self.equity_matrix = np.zeros([HC,HC], dtype=arguments.dtype)
-		if street == SC: # for last round we just return the matrix
-			self.get_last_round_call_matrix(board, self.equity_matrix)
-		elif street == 3 or street == 2: # TODO: can be problems
-			# iterate through all possible next round streets
-			next_round_boards = card_tools.get_last_round_boards(board)
-			BC = next_round_boards.shape[0] # boards_count
-			if self.matrix_mem.ndim != 3 or self.matrix_mem.shape[1] != HC or self.matrix_mem.shape[2] != HC:
-				self.matrix_mem = np.zeros([self.batch_size, HC, HC], dtype=arguments.dtype)
-			self.get_inner_call_matrix(next_round_boards, self.equity_matrix)
-			# averaging the values in the call matrix
-			cards_to_come = BCC[SC] - BCC[street]
-			cards_left = CC - HCC*2 - BCC[street]
-			den = tools.choose(cards_left, cards_to_come)
-			self.equity_matrix *= 1/den
-		elif street == 1:
-			self.equity_matrix = self._pf_equity.copy()
-		else:
-			assert(False) # impossible street
+		equity_matrix[:,:] *= possible_hand_indexes.reshape([HC,1])
+		equity_matrix[:,:] *= self._block_matrix
 
 
 	def get_hand_strengths(self):
@@ -148,9 +97,27 @@ class TerminalEquity():
 			structures.
 		@param: board a possibly empty vector of board cards
 		'''
-		self.board = board
-		self._set_call_matrix(board)
-		self._set_fold_matrix(board)
+		self.board, street, HC = board, card_tools.board_to_street(board), game_settings.hand_count
+		# set call matrix
+		if street == 1:
+			self.equity_matrix = self._pf_equity.copy()
+		elif street == constants.streets_count:
+			self.equity_matrix = np.zeros([HC,HC], dtype=arguments.dtype)
+			self.set_last_round_call_matrix(self.equity_matrix, board)
+			self._handle_blocking_cards(self.equity_matrix, board)
+		elif street == 2 or street == 3:
+			self.equity_matrix = np.zeros([HC,HC], dtype=arguments.dtype)
+			next_round_boards = card_tools.get_last_round_boards(board)
+			self.set_inner_call_matrix(self.equity_matrix, next_round_boards, street)
+			self._handle_blocking_cards(self.equity_matrix, board)
+		else:
+			assert(False) # bad street/board
+		# set fold matrix
+		self.fold_matrix = np.ones([HC,HC], dtype=arguments.dtype)
+		# setting cards that block each other to zero
+		self._handle_blocking_cards(self.fold_matrix, board)
+
+
 
 
 	def call_value(self, ranges, result):
