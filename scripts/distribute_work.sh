59d8c9d8dbafa1eaf54872898bb8b5c8f1c40fd1
doas3140 <dominykas.asauskas@gmail.com>
2018-12-16 22:53:31 +0200

    updated shell scripts to have approximation parameter

--- scripts/distribute_work.sh
+++ scripts/distribute_work.sh
@@ -2,11 +2,12 @@ start_idx=3000
 num_files=3
 num_cores=600
 street=4
+approximate='root_nodes'
 
 total=`expr $num_files \* $num_cores \+ $start_idx`
 
 for idx in `seq $start_idx $num_files $total`
 do
  echo $idx
- sbatch -o ../out/$idx ./run_cfr.sh $street $idx
+ sbatch -o ../out/$idx ./run_cfr.sh $street $idx $approximate
 done
--- scripts/run_cfr.sh
+++ scripts/run_cfr.sh
@@ -2,4 +2,4 @@
 #SBATCH -p short
 #SBATCH -n1
 
-python3 ./generate_data.py --street $1 --start-idx $2
+python3 ./generate_data.py --street $1 --start-idx $2 --approximate $3
